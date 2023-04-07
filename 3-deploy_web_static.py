dc6872ae93a227951e34c0f114686117b962ead9
senseiuc <dhinhojboy@gmail.com>
2023-04-07 12:18:21 +0000

    deploy_web_static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
 """
+a Fabric script that generates a .tgz archive
+from the contents of the web_static folder of
+AirBnB Clone repo, using the function do_pack
+
+"""
+from fabric.api import local
+from time import strftime
+from fabric.api import env, put, run
+import os.path
+
+
+def do_pack():
+    """generate a .tgz archive"""
+    time = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        name = "versions/web_static_{}.tgz".format(time)
+        local("tar -cvzf {} web_static/".format(name))
+        return name
+    except Exception:
+        return None
+
+
+"""
 a Fabric script that deploys a file
 using the 1-pack_web_static.py file
 to pack the file
 
 """
-from fabric.api import env, put, run
-from time import strftime
-import os.path
-
 env.hosts = ['54.237.13.197', '3.83.245.109']
 env.user = 'ubuntu'
 
@@ -32,5 +52,11 @@ def do_deploy(archive_path):
         run("ln -s {} {}".format(f_path, s_link))
         print("New version deployed!")
         return True
-    except ValueError:
+    except Exception:
         return False
+
+
+def deploy():
+    """ deploy file after achiving"""
+    archive_path = do_pack()
+    return do_deploy(archive_path) if archive_path else False
