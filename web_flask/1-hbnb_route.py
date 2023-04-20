62d30bcc7a9267f02724b80728c0dca35abf9888
senseiuc <dhinhojboy@gmail.com>
2023-04-20 12:53:44 +0000

    c is fun

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """
-A simple flask script that displays
-Hello HBNB!
+A simple flask script
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello():
+def index():
     """Returns hello world"""
     return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
