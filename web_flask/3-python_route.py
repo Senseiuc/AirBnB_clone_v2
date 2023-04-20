aacf6a8418ecc92836793c858606e43044a5ec7e
senseiuc <dhinhojboy@gmail.com>
2023-04-20 13:03:14 +0000

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A simple flask script
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Returns hello world"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """show appended text"""
+    return "C %s" % text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def p_python(text = 'is cool'):
+    """print pthon with a text"""
+    return "Python %s" % text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
