f2e74b2df2907a8b318c1878b06ecb5e92c0518a
senseiuc <dhinhojboy@gmail.com>
2023-04-20 12:25:50 +0000

    first flask app

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,20 +17,17 @@ def do_deploy(archive_path):
     """ deploy file"""
     if os.path.isfile(archive_path) is False:
         return False
-    try:
-        put(archive_path, "/tmp/")
-        f_name = archive_path.split("/")[-1]
-        f_n_w_xt = f_name.split(".")[0]
-        f_path = '/data/web_static/releases/{}'.format(f_n_w_xt)
-        s_link = '/data/web_static/current'  # older symlink
-        run("mkdir -p {}".format(f_path))
-        run("tar -xzf /tmp/{} -C {}".format(f_name, f_path))
-        run("rm /tmp/{}".format(f_name))
-        run("mv {}/web_static/* {}".format(f_path, f_path))
-        run("rm -rf {}/web_static".format(f_path))
-        run("rm -rf {}".format(s_link))
-        run("ln -s {} {}".format(f_path, s_link))
-        print("New version deployed!")
-        return True
-    except ValueError:
-        return False
+    put(archive_path, "/tmp/")
+    f_name = archive_path.split("/")[-1]
+    f_n_w_xt = f_name.split(".")[0]
+    f_path = '/data/web_static/releases/{}'.format(f_n_w_xt)
+    s_link = '/data/web_static/current'  # older symlink
+    run("mkdir -p {}".format(f_path))
+    run("tar -xzf /tmp/{} -C {}".format(f_name, f_path))
+    run("rm /tmp/{}".format(f_name))
+    run("mv {}/web_static/* {}".format(f_path, f_path))
+    run("rm -rf {}/web_static".format(f_path))
+    run("rm -rf {}".format(s_link))
+    run("ln -s {} {}".format(f_path, s_link))
+    print("New version deployed!")
+    return True
