0061feef56a8f53f93d4e345e536d9f0503d18b4
senseiuc <dhinhojboy@gmail.com>
2023-04-23 20:28:57 +0000

    images

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,14 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True)
+    id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        id = str(uuid.uuid4())
         if not kwargs:
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
@@ -29,8 +29,6 @@ class BaseModel:
                         value = datetime.strptime(
                             value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, name, value)
-                if 'id' not in kwargs:
-                    setattr(self, 'id', str(uuid.uuid4()))
         if not self.created_at:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
--- models/place.py
+++ models/place.py
@@ -37,22 +37,21 @@ class Place(BaseModel, Base):
         amenity_ids (list of str): List of amenities.
     """
     __tablename__ = "places"
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        amenities = relationship(
-                            "Amenity", secondary=place_amenity, viewonly=False)
-        reviews = relationship("Review", passive_deletes=True, backref="place")
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-    else:
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    amenities = relationship(
+                        "Amenity", secondary=place_amenity, back_populates="place_amenities"
+                        , viewonly=False)
+    reviews = relationship("Review", passive_deletes=True, backref="place")
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         city_id = ""
         user_id = ""
         name = ""
@@ -63,17 +62,7 @@ class Place(BaseModel, Base):
         price_by_night = 0
         latitude = 0.0
         longitude = 0.0
-        amenity_ids = []
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
+        reviews = []
         amenity_ids = []
 
         @property
